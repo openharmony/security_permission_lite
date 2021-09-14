394afa12cf6428fd51386568b7456a3cadfca0f2
Brise.Kael <yinquan2@huawei.com>
2021-09-14 18:09:18 +0800

    Add zip utils to infrastructure.
    Signed-off-by: Brise.Kael <yinquan2@huawei.com>

---
+++ frameworks/permission_standard/permissioninfrastructure/main/cpp/include/zip_utils.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ZIP_UTILS_H
+#define ZIP_UTILS_H
+
+#include <string>
+
+namespace OHOS {
+namespace Security {
+namespace Permission {
+class ZipUtils {
+public:
+    static constexpr int32_t OK = 0;
+
+    static constexpr int32_t ERROR = 1;
+
+    static int32_t CompressString(const std::string& inputStr, std::string& outputStr);
+
+    static int32_t DecompressString(const std::string& inputStr, std::string& outputStr);
+};
+} // namespace Infrastructure
+} // namespace Security
+} // namespace OHOS
+#endif // ZIP_UTILS_H
