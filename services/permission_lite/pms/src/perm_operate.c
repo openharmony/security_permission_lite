4d4038638f8421789c46efb03703d76e3d1a64bc
jiang-xiaofeng <jiangxiaofeng8@huawei.com>
2022-03-20 15:43:27 +0800

    fix reviewbot Signed-off-by: jiang-xiaofeng <jiangxiaofeng8@huawei.com>
    Signed-off-by: jiang-xiaofeng <jiangxiaofeng8@huawei.com>

--- services/permission_lite/pms/src/perm_operate.c
+++ services/permission_lite/pms/src/perm_operate.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020~2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2020~2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -70,7 +70,7 @@ void DeleteTask(TList *list, int uid)
     while (cur != NULL) {
         if (cur->uid == uid) {
             if (pre == NULL) {
-                list->head =  cur->next;
+                list->head = cur->next;
             } else {
                 pre->next = cur->next;
             }
